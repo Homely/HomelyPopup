7ec66463f2ee7286a83084f6d3c6c78a99e393cb
Ryan Miranda <rpmir1@gmail.com>
2018-03-30 14:18:01 +1100

    Initial commit with empty ASP.NET Core 2 API.

---
+++ HomelyPopup.AspNetCore/HomelyPopup.AspNetCore/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomelyPopup.AspNetCore.Controllers
+{
+    [Route("api/orders")]
+    public class OrdersController : Controller
+    {
+        // GET api/orders?customerName="Ryan"
+        [HttpGet]
+        public string Get(string customerName)
+        {
+            return "TODO";
+        }
+    }
+}
