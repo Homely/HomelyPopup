30859a96ad5daaf650f7de00c9ad05511ac659eb
Ryan Miranda <rpmir1@gmail.com>
2018-03-30 14:35:19 +1100

    Added AspNetWeb project

---
+++ HomelyPopup.AspNetWeb/HomelyPopup.AspNetWeb/Controllers/OrdersController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace HomelyPopup.AspNetWeb.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        // GET api/orders?customerName="Ryan"
+        public string Get(string customerName)
+        {
+            return "TODO";
+        }
+    }
+}
---
+++ HomelyPopup.AspNetWeb/HomelyPopup.AspNetWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomelyPopup.AspNetWeb.WebApiApplication" Language="C#" %>
